5e60c05e7d05a82f3c8e29d80f775d8df24e7e95
Daneshpm <danesh12112002@gmail.com>
2026-07-01 11:34:52 +0530

    new commits

---
+++ Bitsset.java
@@ -0,0 +1,23 @@
+package datastructures;
+import java.util.*;
+public class Bitsset {
+public static void main(String[] args) {
+	int n=5;
+//	List<Integer> s = new ArrayList<Integer>();
+//	for(int i=0;i*i<=n;i++) {
+//		int ans =(int) Math.pow(2,i);
+//		
+//		s.add(ans);
+//	}
+//	System.out.println(s);
+//	int count=0;
+//	while(n>=1) {
+//	if(n%2!=0) {
+//		count++;
+//	}
+//	n/=2;
+//	}
+//	System.out.println(count);
+	
+}
+}
